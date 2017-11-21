e1ccbdae42a38c7fd993a91877943dac52041458
PingjunChen <chenpingjun@gmx.com>
2017-11-21 16:07:51 -0500

    add MNIST pytorch example

---
+++ mnist-pytorch/loader.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import torch.utils.data
+from torchvision import datasets, transforms
+
+
+def train_loader(args):
+    kwargs = {"num_workers": 1, "pin_memory": True} if args.cuda else {}
+    loader = torch.utils.data.DataLoader(
+        datasets.MNIST("./data", train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
+
+    return loader
+
+
+def test_loader(args):
+    kwargs = {"num_workers": 1, "pin_memory": True} if args.cuda else {}
+    loader = torch.utils.data.DataLoader(
+        datasets.MNIST("./data", train=False,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=args.test_batch_size, shuffle=False, **kwargs)
+
+    return loader
---
+++ mnist-pytorch/train_mnist.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+import os, sys, pdb
+import argparse, shutil
+import torch
+import torch.optim as optim
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+from network import Net
+from loader import train_loader
+
+
+def set_args():
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    # mini_batch and epoch information
+    parser.add_argument('--batch-size',      type=int,   default=64)
+    parser.add_argument('--epochs',          type=int,   default=10)
+    # Optimization parameters
+    parser.add_argument('--lr',              type=float, default=0.01)
+    parser.add_argument('--momentum',        type=float, default=0.9)
+    #
+    parser.add_argument('--seed',            type=int,   default=1)
+    parser.add_argument('--device_id',       type=int,   default=0)
+    parser.add_argument('--log-interval',    type=int,   default=10)
+    # model directory and name
+    parser.add_argument('--model-dir',       type=str,   default="./models")
+    parser.add_argument('--model-name',      type=str,   default="mnist")
+
+    args = parser.parse_args()
+    return args
+
+
+def train(data_loader, model, args):
+    model.train()
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+
+    if os.path.exists(args.model_dir):
+        shutil.rmtree(args.model_dir)
+    os.makedirs(args.model_dir)
+
+    for epoch in range(1, args.epochs+1):
+        for batch_idx, (data, target) in enumerate(data_loader):
+            if args.cuda:
+                data, target = data.cuda(), target.cuda()
+            data, target = Variable(data), Variable(target)
+            optimizer.zero_grad()
+            output = model(data)
+            loss = F.nll_loss(output, target)
+            loss.backward()
+            optimizer.step()
+            if batch_idx > 0 and batch_idx % args.log_interval == 0:
+                print("Train Epoch: {} [{}/{} ({:.0f})%)]\t Loss: {:.6f}".format(
+                    epoch, batch_idx * len(data), len(data_loader.dataset),
+                    100. * batch_idx / len(data_loader), loss.data[0]
+                ))
+        cur_model_name = args.model_name + "-" + str(epoch).zfill(2) + ".pth"
+        torch.save(model.state_dict(), os.path.join(args.model_dir, cur_model_name))
+        print('Save weights at {}/{}'.format(args.model_dir, cur_model_name))
+
+
+if __name__ == '__main__':
+    args = set_args()
+    # Config model and gpu
+    args.cuda = torch.cuda.is_available()
+    torch.manual_seed(args.seed)
+    model = Net()
+    if args.cuda:
+        torch.cuda.manual_seed(args.seed)
+        model.cuda(args.device_id)
+        import torch.backends.cudnn as cudnn
+        cudnn.benchmark = True
+
+    # dataloader
+    data_loader = train_loader(args)
+
+    # Start training
+    train(data_loader, model, args)
